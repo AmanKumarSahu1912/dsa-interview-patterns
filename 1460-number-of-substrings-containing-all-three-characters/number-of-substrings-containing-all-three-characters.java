4157366acbef676d49a21621399cf164ee41093c
Aman Kumar Sahu <88515196+AmanKumarSahu1912@users.noreply.github.com>
2026-08-23 04:30:40 +0530

    Time: 14 ms (57.14%) | Memory: 46.1 MB (90.50%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -8,6 +8,7 @@ class Solution {
             lastseen[s.charAt(right)-'a']=right;
             //min from all 3
             int minindex=Math.min(lastseen[0],Math.min(lastseen[1],lastseen[2]));
+            //we can also skip if condition and directly set count+=minindex+1(ex -1+1=0)
             if(minindex!=-1)
             {
                 count+=minindex+1;

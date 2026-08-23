885aa1d2722d7fdae34134bcb0ba51608983aa27
Aman Kumar Sahu <88515196+AmanKumarSahu1912@users.noreply.github.com>
2026-08-23 05:54:35 +0530

    Time: 2 ms (99.74%) | Memory: 61.7 MB (73.56%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -11,9 +11,14 @@ class Solution {
             left++;
         }
         maxpoints=leftsum;
-        for(left=k-1;left>=0;left--)
+        //for(left=k-1;left>=0;left--)
+        while(k>0)
         {
-            leftsum-=cardPoints[left];
+           /* leftsum-=cardPoints[left];
+            rightsum+=cardPoints[right];
+            right--;*/
+            leftsum-=cardPoints[k-1];
+            k--;
             rightsum+=cardPoints[right];
             right--;
             maxpoints=Math.max(maxpoints,leftsum+rightsum);
